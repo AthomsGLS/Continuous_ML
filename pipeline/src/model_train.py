e66e5bfd977bb71da88a48b65f632486cf0e450c
Thomas Gaehtgens <151099706+AthomsGLS@users.noreply.github.com>
2024-03-02 00:50:46 +0000

    Increased Random Forest max depth

--- pipeline/src/model_train.py
+++ pipeline/src/model_train.py
@@ -2,7 +2,7 @@ from sklearn.ensemble import RandomForestRegressor
 
 def model_train(X_train, y_train, seed):
     # Fit a model on the train section
-    regr = RandomForestRegressor(max_depth=2, random_state=seed)
+    regr = RandomForestRegressor(max_depth=8, random_state=seed)
     regr.fit(X_train, y_train)
 
     return regr
